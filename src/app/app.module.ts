8fc1603c9fa9e5d3096522ccb0f0e63554f37140
philipiaeveline <philipiaeveline@gmail.com>
2020-11-03 22:01:22 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { GoalFormComponent } from './goal-form/goal-form.component';
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+  
+    GoalComponent,
+    StrikethroughDirective,
+    DateCountPipe,
+    GoalFormComponent
+  
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
